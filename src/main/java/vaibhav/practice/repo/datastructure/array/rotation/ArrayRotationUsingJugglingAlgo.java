94989c91f08e152e47b4c67a01a7a7646a4b841e
vaibhav.soni <sonivaibhav23@gmail.com>
2020-08-27 12:00:17 +0530

    [Add] - added code for graph and array data structure

---
+++ src/main/java/vaibhav/practice/repo/datastructure/array/rotation/ArrayRotationUsingJugglingAlgo.java
@@ -0,0 +1,53 @@
+package vaibhav.practice.repo.datastructure.array.rotation;
+
+public class ArrayRotationUsingJugglingAlgo {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+		ArrayRotationUsingJugglingAlgo arrayRotation = new ArrayRotationUsingJugglingAlgo();
+		int d = 3;
+		arrayRotation.rotateLeft(arr, d, arr.length);
+		System.out.println("Rotated Array by " + d);
+		arrayRotation.printArray(arr);
+	}
+
+	public void rotateLeft(int[] arr, int d, int n) {
+		d = d % n;
+
+		int gcd = gcd(n, d);
+
+		for (int i = 0; i < gcd; i++) {
+			int j = i;
+			int temp = arr[i];
+
+			while (true) {
+				int k = j + d;
+
+				if (k >= n) {
+					k = k - n;
+				}
+				if (k == i) {
+					break;
+				}
+				arr[j] = arr[k];
+				j = k;
+			}
+			arr[j] = temp;
+		}
+	}
+
+	public int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+
+	public void printArray(int[] arr) {
+		for (int a : arr) {
+			System.out.print(a + " ");
+		}
+	}
+
+}
---
+++ src/main/java/vaibhav/practice/repo/datastructure/array/search/SearchElementInRotateArray.java
@@ -0,0 +1,37 @@
+package vaibhav.practice.repo.datastructure.array.search;
+
+public class SearchElementInRotateArray {
+
+	public static void main(String[] args) {
+		int[] arr = { 5, 6, 7, 1, 2, 3, 4 };
+		int x = 9;
+
+		SearchElementInRotateArray rotatedArray = new SearchElementInRotateArray();
+		int i = rotatedArray.search(arr, x, 0, arr.length - 1);
+		if (i < 0)
+			System.out.println("Element Not Found!!!");
+		else
+			System.out.println("Element found at index " + i);
+	}
+
+	private int search(int[] arr, int x, int beg, int end) {
+		if (beg == end && arr[beg] != x)
+			return -1;
+
+		int mid = (beg + end) / 2;
+
+		if (arr[mid] == x)
+			return mid;
+
+		if (arr[mid] > x) {
+			if (arr[beg] <= x)
+				return search(arr, x, beg, mid - 1);
+			return search(arr, x, beg, mid - 1);
+		} else {
+			if (arr[end] >= x)
+				return search(arr, x, mid + 1, end);
+			return search(arr, x, beg, mid - 1);
+		}
+	}
+
+}
